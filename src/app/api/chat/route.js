7bf9a6f3620355f1591b47c9eeb233565f33439e
ronie2020 <ronie.rodiana@gmail.com>
2025-07-10 16:32:07 +0700

    fix: chat/route.js

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -27,7 +27,7 @@ export async function POST(request) {
         
         const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
         
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
         
         const cleanHistory = history.filter(msg => msg.parts[0].text.trim() !== '');
 
